7fcd67069891b156aa76c54303f7fdd8e2ff3709
Guillaume Charbonnier <guillaume.charbonnier@outlook.com>
2019-10-07 14:05:16 +0200

    add triple tries for compilation

--- compile-slides.sh
+++ compile-slides.sh
@@ -3,10 +3,9 @@
 TARGET="out/tectonic/ln/updir/mw-gcthesis-oral/slides.pdf out/tectonic/ln/updir/mw-gcthesis-oral/slides_with_notes.pdf"
 #n=0
 #until [ $n -ge 3 ]
-#for i in 1 2 3;
-#do
-snakemake $TARGET --cores 2 --use-conda
-# && break  # substitute your command here
-#done
+for i in 1 2 3;
+do
+	snakemake $TARGET --cores 2 --use-conda && break 
+done
 
 #ln -sf $TARGET
